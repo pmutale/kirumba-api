e78dd97c1a68fd817df68054a192880cd4b90f99
P. Mutale <peter.mutale@energyworx.org>
2020-02-28 14:04:05 +0100

    Tweak server load

--- apiserver/openapi_server/main.py
+++ apiserver/openapi_server/main.py
@@ -1,4 +1,16 @@
 from openapi_server import app
 
+import googlecloudprofiler
+
+# Profiler initialization. It starts a daemon thread which continuously
+# collects and uploads profiles. Best done as early as possible.
+try:
+    # service and service_version can be automatically inferred when
+    # running on App Engine. project_id must be set if not running
+    # on GCP.
+    googlecloudprofiler.start(verbose=3)
+except (ValueError, NotImplementedError) as exc:
+    print(exc)  # Handle errors here
+
 if __name__ == '__main__':
     app.run(port=7070)
