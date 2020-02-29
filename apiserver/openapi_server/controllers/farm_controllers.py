98276e928915d48b9363805446cc7e83ae207796
P. Mutale <peter.mutale@energyworx.org>
2020-02-29 01:39:39 +0100

    Update Open API Spec

---
+++ apiserver/openapi_server/controllers/farm_controllers.py
@@ -0,0 +1,30 @@
+def add_farm():
+    pass
+
+
+def add_new_lot():
+    pass
+
+
+def get_farm():
+    pass
+
+
+def record_pick_routine():
+    pass
+
+
+def get_lot():
+    pass
+
+
+def edit_routine():
+    pass
+
+
+def delete_farm():
+    pass
+
+
+def get_egg():
+    pass
